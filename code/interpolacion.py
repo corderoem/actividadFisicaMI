6855c42cab74e2e2900e1e32bb801cf90a3d5b87
eacorsab <corderoemss@gmail.com>
2024-01-09 13:51:58 -0500

    Archivo de interpolacion

---
+++ code/interpolacion.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+from pykrige.ok3d import OrdinaryKriging3D
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+file_path = 'data/datos-errores.xlsx' 
+df = pd.read_excel(file_path)
+
+data = df[['x', 'y', 'z', 'errorTotal', 'errorX', 'errorY', 'errorZ']].values
+
+# Función para crear un modelo de Kriging 3D
+def crear_modelo_kriging(coord_x, coord_y, coord_z, error):
+    return OrdinaryKriging3D(
+        coord_x, coord_y, coord_z,
+        error,
+        variogram_model='spherical',
+        verbose=False
+    )
+
+# Crear modelos de Kriging para cada eje
+modelo_krigingX = crear_modelo_kriging(data[:, 0], data[:, 1], data[:, 2], data[:, 4])
+modelo_krigingY = crear_modelo_kriging(data[:, 0], data[:, 1], data[:, 2], data[:, 5])
+modelo_krigingZ = crear_modelo_kriging(data[:, 0], data[:, 1], data[:, 2], data[:, 6])
+modelo_krigingTotal = crear_modelo_kriging(data[:, 0], data[:, 1], data[:, 2], data[:, 3])
+
+new_x = float(input("Ingrese la coordenada x: "))
+new_y = float(input("Ingrese la coordenada y: "))
+new_z = float(input("Ingrese la coordenada z: "))
+
+# Predicciones de errores en cada eje utilizando Kriging
+errorX_interpolado, _ = modelo_krigingX.execute('grid', [new_x], [new_y], [new_z])
+errorY_interpolado, _ = modelo_krigingY.execute('grid', [new_x], [new_y], [new_z])
+errorZ_interpolado, _ = modelo_krigingZ.execute('grid', [new_x], [new_y], [new_z])
+errorTotal_interpolado, _ = modelo_krigingTotal.execute('grid', [new_x], [new_y], [new_z])
+
+print(f"Error X interpolado en ({new_x}, {new_y}, {new_z}): {errorX_interpolado[0][0]}")
+print(f"Error Y interpolado en ({new_x}, {new_y}, {new_z}): {errorY_interpolado[0][0]}")
+print(f"Error Z interpolado en ({new_x}, {new_y}, {new_z}): {errorZ_interpolado[0][0]}")
+print(f"Error total interpolado en ({new_x}, {new_y}, {new_z}): {errorTotal_interpolado[0][0]}")
+'''
+data = df[['x', 'y', 'z', 'error']].values
+
+#Crear un modelo de Kriging 3D
+ok3d = OrdinaryKriging3D(
+    data[:, 0],  # x
+    data[:, 1],  # y
+    data[:, 2],  # z
+    data[:, 3],  # error
+    variogram_model='spherical',
+    verbose=False
+)
+
+# Pedir al usuario que ingrese las coordenadas
+new_x = float(input("Ingrese la coordenada x: "))
+new_y = float(input("Ingrese la coordenada y: "))
+new_z = float(input("Ingrese la coordenada z: "))
+
+#print(data)
+
+prediction, variance = ok3d.execute('grid', [new_x], [new_y], [new_z])
+
+#print(ok3d.variogram_model_parameters)
+#print(ok3d.variogram_function)
+
+print(f"Valor de predicción en ({new_x}, {new_y}, {new_z}): {prediction[0][0]}")
+print(f"Incertidumbre en la predicción: {variance[0][0]}")
+'''
